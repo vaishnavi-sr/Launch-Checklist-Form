20dcbfcc5348011be5c98e2f965b08d8fb5a4fd4
vaishnavi <vaishu.srini15@gmail.com>
2021-06-16 06:20:54 -0500

    My first commit

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // Write your JavaScript code here!
-
+window.addEventListener("load", function() {
+   let form = document.querySelector("form");
+   let list;
+   list = document.querySelector("#faultyItems");
+   list.style.visibility = 'hidden';
+});
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
